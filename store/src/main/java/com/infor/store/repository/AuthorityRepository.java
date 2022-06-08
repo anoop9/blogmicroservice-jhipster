c769c2817cac552fc4633b4df2ad6b9120d838b8
Anoop Ambooken <anoop.ambooken@infor.com>
2022-06-08 20:24:28 +0530

    initial commit
    initial commit

---
+++ store/src/main/java/com/infor/store/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.infor.store.repository;
+
+import com.infor.store.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ store/src/main/java/com/infor/store/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.infor.store.service.dto;
